37bdc8d2f2d88e9349ba04bc41df81a33ab7e50e
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2022-12-26 20:55:09 +0530

    added auth-sign In

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
-// import authRoutes from "./routes/auth.js";
+import authRoutes from "./routes/auth.js";
 // import userRoutes from "./routes/users.js";
 // import postRoutes from "./routes/posts.js";
 import { home, register } from "./controllers/auth.js";
@@ -50,12 +50,13 @@ app.get("/auth/home", home)
 // app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
-// app.use("/auth", authRoutes);
+app.use("/auth", authRoutes);
 // app.use("/users", userRoutes);
 // app.use("/posts", postRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
+mongoose.set("strictQuery", false);
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
