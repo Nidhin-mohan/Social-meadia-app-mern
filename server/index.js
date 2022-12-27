7ddf2c5d536d20e4962c632ce854bdb708a24672
Nidhin-mohan <nidhinmohannidhin@gmail.com>
2022-12-27 11:42:36 +0530

    added some fake user data to db

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
 import User from "./models/User.js";
 import Post from "./models/Post.js";
-// import { users, posts } from "./data/index.js";
+import { users, posts } from "./data/index.js";
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -66,7 +66,7 @@ mongoose
     app.listen(PORT, () =>  console.log(`App is running at http://localhost:${PORT}`));
 
     /* ADD DATA ONE TIME */
-    // User.insertMany(users);
-    // Post.insertMany(posts);
+    User.insertMany(users);
+    Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
